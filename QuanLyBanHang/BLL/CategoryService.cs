1602faee91b7ed7ad49f110a300554414f42b325
Theanhmgt66 <theanhmgt66@gmail.com>
2024-05-07 23:11:29 +0700

    feat(app): add first api for category model, add swagger libraty

---
+++ QuanLyBanHang/BLL/CategoryService.cs
@@ -0,0 +1,25 @@
+﻿using DAL;
+using DAL.Models;
+using QLBH.Common.BLL;
+using QLBH.Common.Rsp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace BLL
+{
+    public class CategoryService: GenericSvc<CategoryRepository,Category>
+    {
+        private CategoryRepository categoryRepostiory;
+        public CategoryService() {
+            categoryRepostiory = new CategoryRepository();
+        }
+        public override SingleRsp Read(int id)
+        {
+            var res = new SingleRsp();
+            res.Data = _rep.Read(id);
+            return res;
+        }
+    }
+}
