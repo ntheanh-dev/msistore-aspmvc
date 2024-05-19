17e359a13b6d56a0431ed5a30e37dc843b233d5b
MinhTamNT <minhtam78945@gmail.com>
2024-05-19 13:30:37 +0700

    update User Controller

---
+++ QuanLyBanHang/MSISTORE.WEB/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSISTORE.WEB.Controllers
+{
+    public class UserController : Controller
+    {
+
+        [HttpPost("create-user")]
+        public IActionResult CreateUser()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
