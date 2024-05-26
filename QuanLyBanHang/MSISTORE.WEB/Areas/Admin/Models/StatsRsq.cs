9044fa294107c72b8b696a5ef973894efaf5f0b3
Sinh Hùng <sinhhung.ng@gmail.com>
2024-05-26 16:11:04 +0700

    admin stats

---
+++ QuanLyBanHang/MSISTORE.WEB/Areas/Admin/Models/StatsRsq.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSISTORE.WEB.Areas.Admin.Models
+{
+    public class StatsRsq
+    {
+        private long Year { get; set; }
+        private string Type { get; set; }
+
+    }
+}
