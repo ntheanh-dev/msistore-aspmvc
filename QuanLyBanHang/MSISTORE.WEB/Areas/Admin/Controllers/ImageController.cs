679c923ff93a3acd15e14812f29e7919ab5d4ff6
MinhTamNT <minhtam78945@gmail.com>
2024-05-26 14:03:12 +0700

    feature : admin site manage image for the product

---
+++ QuanLyBanHang/MSISTORE.WEB/Areas/Admin/Controllers/ImageController.cs
@@ -0,0 +1,22 @@
+﻿using DAL.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MSISTORE.WEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ImageController : Controller
+    {
+        msistoreContext da = new msistoreContext();
+
+        public IActionResult Index()
+        {
+            var ds = da.Images.Include(p=> p.Prodcut).OrderByDescending(p => p.Id).ToList();
+            return View(ds);
+        }
+        public IActionResult CreateImageProduct()
+        {
+            return View();
+        }
+    }
+}
